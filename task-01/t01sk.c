6b1e5c7f8c67e5a36335d54b9d96f15c33ecea6a
frasercrossman <frasercrossman@outlook.com>
2017-04-22 17:57:02 +0000

    Recommented task 1

--- task-01/t01sk.c
+++ task-01/t01sk.c
@@ -16,9 +16,9 @@
 #include <avr/interrupt.h>
 
 
-#define STEP_DELAY_MS 5
-#define MIN_STEP    2    /* > 0 */
-#define MAX_STEP  255
+#define STEP_DELAY_MS 	5
+#define MIN_STEP   	2    /* > 0 */
+#define MAX_STEP  	255
 
 void init(void);
 
@@ -56,16 +56,25 @@ void main(void) {
 
 /* Configure I/O Ports */
 void init(void) {
+	
+	/*
+		To avoid unintentional changes of clock frequency, a special write procedure
+		must be followed to change the CLKPS bits:
+		1. Write the Clock Prescaler Change Enable (CLKPCE) bit to one and all other
+		   bits in CLKPR to zero
+		2. Within four cycles, write the desired value to CLKPS while writing a zero
+		   to CLKPCE
+	*/
 
 	/* 8MHz clock, no prescaling (DS, p. 48) */
 	CLKPR = (1 << CLKPCE); // Enables Clock Prescaler Change Enable
 	CLKPR = 0; // Sets Clock Prescale Register to all zeros (Disables CLKPCE)
+		   // Division factor = 1 (DS, p. 48)
 
 
-    /* Configure I/O Ports */
-
-	DDRB  |=  _BV(PB7);   /* LED pin out */
-	PORTB &= ~_BV(PB7);   /* LED off */
+	/* Configure I/O Ports DS, p. 71 */
+	DDRB  |=  _BV(PB7);   /* LED pin set to output mode */
+	PORTB &= ~_BV(PB7);   /* LED pin off - LED turns off*/
 
 	/* DDRE &= 1100 1111 */
 	DDRE &= ~_BV(PE4) & ~_BV(PE5); /* Rotary Encoder Data Driven Direction set to Input */	
@@ -73,17 +82,25 @@ void init(void) {
 	PORTE |= _BV(PE4) | _BV(PE5); /* Rotary Encoder Port E Activate Pull-up Resistors */
 
 	/* Timer 0 for switch scan interrupt: */
-
-	TCCR0A = _BV(WGM01);
-	TCCR0B = _BV(CS01)
-          | _BV(CS00);   /* F_CPU / 64 */
+	TCCR0A = _BV(WGM01); 		/* Waveform Generation Mode 2 */
+					/* Timer/Counter mode of operation: CTC (Compare Match) */
+					/* If OCR0A is > Timer MAX then TOV flag is set (Timer Overflow) */
+	TCCR0B = _BV(CS01) | _BV(CS00); /* F_CPU / 64 (Timer increment frequency) */
           
-    // OCR0A Output Compare Register 0A (8-bit register)
-    /* SET OCR0A FOR A 1 MS PERIOD */        
-    OCR0A = F_CPU / 2000 - 1;
+    	// OCR0A Output Compare Register 0A (8-bit register)
+    	/* SET OCR0A FOR A 1 MS PERIOD */ 
+    	OCR0A = F_CPU / 64000 - 1;
 
-    /* ENABLE TIMER INTERRUPT */
-    TIMSK0 |= _BV(OCIE0A);
+	/*
+		The timer works by incrementing it's value until it reaches the value
+		specified by OCR0A. Then it calls the ISR and resets.
+
+		NOTE: ISR is only called if the 'Output Compare Flag' is set.
+		      The generated interrupt is known as an 'Output Compare Interrupt'
+	*/
+
+    	/* ENABLE TIMER INTERRUPT */
+    	TIMSK0 |= _BV(OCIE0A);
 }
 
 
@@ -104,11 +121,10 @@ void init(void) {
      wheel = PINE;
      new = 0;
      if( wheel  & _BV(PE4) ) new = 3;
-     if( wheel  & _BV(PE5) )
-	 new ^= 1;		        	/* convert gray to binary */
+     if( wheel  & _BV(PE5) ) new ^= 1;  /* convert gray to binary */
      diff = last - new;			/* difference last - new  */
      if( diff & 1 ){			/* bit 0 = value (1) */
-	     last = new;		       	/* store new as next last  */
+	     last = new;		/* store new as next last  */
 	     delta += (diff & 2) - 1;	/* bit 1 = direction (+/-) */
      }
 
